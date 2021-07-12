36a35b6602066fe99bd8e27d4af51f228027719b
Pawel Palka <pawel.palka@persgroep.net>
2021-07-12 13:13:28 +0200

    fix tetsing url

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: false,
-  appUrl: 'http://localhost:3000'
+  appUrl: 'https://notification-dashboard.newsletter.cust.aws.berlingskemedia-testing.net'
 };
 
 /*
