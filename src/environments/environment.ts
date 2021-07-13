038403621bcd01a529ae7e8846d0f9971b75beb4
Pawel Palka <pawel.palka@persgroep.net>
2021-07-13 13:25:18 +0200

    fix urls

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: false,
-  appUrl: 'https://notification-dashboard.newsletter.cust.aws.berlingskemedia-testing.net'
+  appUrl: 'http://localhost:3000'
 };
 
 /*
