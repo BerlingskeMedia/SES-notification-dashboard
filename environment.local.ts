99e660800bd50ff5d459ef92b55ec195f51e3834
Pawel Palka <pawel.palka@persgroep.net>
2021-07-13 13:22:02 +0200

    remove local urls stuff

--- environment.local.ts
+++
@@ -1,7 +0,0 @@
-// This file is replacing src/environments/environment.ts during mounting container locally
-
-export const environment = {
-  production: false,
-  appUrl: 'http://localhost:3000'
-};
-
